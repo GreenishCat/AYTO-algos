a5d4b56bc32c941646ad447eda0be421272709b2
Erica Davies <98404604+GreenishPixie@users.noreply.github.com>
2024-04-23 13:28:37 -0400

    Update AYTO Algorithms.py
    updated the greedy algorithm and added more comments

--- AYTO Algorithms.py
+++ AYTO Algorithms.py
@@ -3,6 +3,7 @@ from itertools import combinations
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import statistics
 
 class AYTO_Game:
     """game set up for the algorithms to use"""
@@ -10,7 +11,7 @@ class AYTO_Game:
         """initializes variables and the constestants to pair"""
         self.contestants = list(range(0,16)) # creates the list of contestants, numbered 0 - 15
         random.shuffle(self.contestants) # shuffles the list of contestants
-        self.perfect_pairs = [(self.contestants[i], self.contestants[i+1]) for i in range(0, 16, 2)] # creates pairs
+        self.perfect_pairs = [tuple(sorted((self.contestants[i], self.contestants[i+1]))) for i in range(0, 16, 2)] # creates pairs
         self.found_pairs = []
         self.guesses = []
         self.available_contestants = set(range(0,16))
@@ -18,13 +19,13 @@ class AYTO_Game:
     def new_round(self, guessed_pairs):
         """code for what happens when round is over/start of new round"""
         self.guesses.append(guessed_pairs) # adds guessed pair to the list of guesses
-        correct_count = sum(1 for pair in guessed_pairs if pair in self.perfect_pairs) # increases count of correct pairs if the pair was correct
+        correct_count = sum(1 for pair in guessed_pairs if tuple(sorted(pair)) in self.perfect_pairs) # increases count of correct pairs if the pair was correct
         #print(f"correct_count: {correct_count}\nfound pairs: {self.found_pairs}\n----")
         return correct_count # returns how many pairs were correct from the given guessed pairs
     
     def truth_booth(self, pair):
         """truth booth code, determines if a pair is correct or not"""
-        #pair = tuple(sorted(pair))
+        pair = tuple(sorted(pair))
         result = pair in self.perfect_pairs # checks if the chosen pair is in the list of perfect pairs
         if result == True: # if it is, adds it to list of found pairs
             self.found_pairs.append(pair)
@@ -38,6 +39,7 @@ class AYTO_Game:
     
 def pair_contestants(available_contestants):
     """pairs the contestants together"""
+    available_contestants = list(available_contestants)
     random.shuffle(available_contestants)
     pairs = [(available_contestants[i], available_contestants[i+1]) for i in range(0, len(available_contestants) - 1, 2)]
     return pairs
@@ -57,45 +59,54 @@ def random_brute_force_alg(game):
 
 def greedy_alg(game):
     rounds = 0
-    potential_pairs = set(combinations(game.available_contestants, 2))
+    potential_pairs = set(combinations(game.available_contestants, 2)) # creates a set of all potential pairs
 
     while not game.is_game_over():
-        if rounds == 0 or correct_count == 0: # on the first round, the alg doesn't have any information, so it randomly picks a pair
-            pairs = pair_contestants(list(game.available_contestants))
-        else:
+        if rounds != 0 and correct_count == 0:
+            for pair in pairs:
+                potential_pairs.difference_update(pair) # since all the pairs in the list are incorrect, removes all of them from the list of potential pairs
+
+        if rounds == 0 or correct_count == 0: # on the first round or if all the pairs in a previous list were wrong
+            pairs = pair_contestants(list(game.available_contestants)) # creates a new list of pairs randomly
+        else: # if it's not the first round
             pairs = []
-            used_contestants = set()
-            for pair in sorted(potential_pairs, key=lambda x: -pair_score(x, game.found_pairs)):
-                if pair[0] not in used_contestants and pair[1] not in used_contestants:
+            used_contestants = set() # set to keep track of what pairs have been used
+            for pair in sorted(potential_pairs, key=lambda x: -pair_score(x, game.found_pairs)): # iterates over potential pairs, sorting them based on their pair score
+                if pair[0] not in used_contestants and pair[1] not in used_contestants: # checks if neither contestant in the pair has been used yet in this round
                     pairs.append(pair)
                     used_contestants.update(pair)
-                    if len(used_contestants) >= len(game.available_contestants):
+                    if len(used_contestants) >= len(game.available_contestants): # checks if all available contestants have been used
                         break
 
-        correct_count = game.new_round(pairs)
+        correct_count = game.new_round(pairs) # starts new round
+        rounds += 1
 
-        if not pairs:
+        if not pairs: # checks if no pairs were formed
             remaining_contestants = list(game.available_contestants - used_contestants)
             if len(remaining_contestants) == 2:
                 tb_pair = tuple(remaining_contestants)
                 tb_result = game.truth_booth(tb_pair)
             continue
 
-        if pairs:
-            tb_pair = max(pairs, key=lambda x: pair_score(x, game.found_pairs))
-            tb_result = game.truth_booth(tb_pair)
+        if pairs: # if pairs were formed
+            tb_pair = max(pairs, key=lambda x: pair_score(x, game.found_pairs)) # selects best pair based on pair score
+            #print(tb_pair)
+            tb_result = game.truth_booth(tb_pair) # sends pair to truth booth
 
-            if tb_result:
-                potential_pairs.difference_update(pairs_that_cannot_be(tb_pair, potential_pairs))
-            else:
-                potential_pairs.discard(tb_pair)
+            if tb_result == True:
+                #print(f"found pair: {tb_pair}")
+                potential_pairs.difference_update(pairs_that_cannot_be(tb_pair, potential_pairs)) # updates set of all potential pairs
+            elif tb_result == False:
+                potential_pairs.discard(tb_pair) # removes the tested pair from potential pairs since it's incorrect
 
-        rounds += 1
+    print(game.found_pairs)
     return rounds
 
 def pair_score(pair, found_pairs):
+    """"""
     pair = tuple(sorted(pair))
-    if pair in found_pairs:
+    if pair in found_pairs: # if pair already found to be correct, deprioritizes it
+        #print(f"pair found: {pair}")
         return -1
     else:
         return 1
@@ -118,60 +129,73 @@ rounds_taken = greedy_alg(game)
 #rounds_taken = optimized_algorithm(game)
 print(rounds_taken)
 
+round_sample = [] # rounds taken for each game will be appended here to be plotted later.
+for new_game in range(10000):
+    game = AYTO_Game()
+    rounds_taken = greedy_alg(game)
+    print(rounds_taken)
+    round_sample.append(rounds_taken)
+# Creates a histrogram of rounds taken.
+median_value = statistics.median(round_sample)
+mean_value = statistics.mean(round_sample)
+sns.histplot(round_sample).set(title='greedy', xlabel='Rounds')
+plt.figtext(0.5, 0.7, f'Median: {median_value}\nMean: {mean_value}', ha="left", fontsize=12, bbox={"facecolor":"white", "alpha":0.5, "pad":5})
+# Saves histogram to working directory.
+plt.savefig('greedy fhsjfa line')
+plt.close()
 
 
-
-data = np.random.randn(1000)
+# data = np.random.randn(1000)
  
-# Plotting a basic histogram
-plt.hist(data, bins=30, color='skyblue', edgecolor='black')
+# # # Plotting a basic histogram
+# # plt.hist(data, bins=30, color='skyblue', edgecolor='black')
  
-# Adding labels and title
-plt.xlabel('Values')
-plt.ylabel('Frequency')
-plt.title('Basic Histogram')
+# # # Adding labels and title
+# # plt.xlabel('Values')
+# # plt.ylabel('Frequency')
+# # plt.title('Basic Histogram')
  
-# Display the plot
-plt.show()
+# # # Display the plot
+# # plt.show()
 
-def main():
-    """ Runs all three algorithms and outputs, to the working directory,
-     a png histrogram file of rounds taken for each algorithm."""
+# def main():
+#     """ Runs all three algorithms and outputs, to the working directory,
+#      a png histrogram file of rounds taken for each algorithm."""
     
-    round_sample = [] # rounds taken for each game will be appended here to be plotted later.
-    for new_game in range(10000):
-        game = AYTO_Game()
-        rounds_taken = random_brute_force_alg(game)
-        round_sample.append(rounds_taken)
-    # Creates a histrogram of rounds taken.
-    sns.histplot(round_sample).set(title='Random Brute Force Algorithm', xlabel='Rounds')
-    # Saves histogram to working directory.
-    plt.savefig('brute_force_histogram')
-    plt.close()
+#     round_sample = [] # rounds taken for each game will be appended here to be plotted later.
+#     for new_game in range(10000):
+#         game = AYTO_Game()
+#         rounds_taken = random_brute_force_alg(game)
+#         round_sample.append(rounds_taken)
+#     # Creates a histrogram of rounds taken.
+#     sns.histplot(round_sample).set(title='Random Brute Force Algorithm', xlabel='Rounds')
+#     # Saves histogram to working directory.
+#     plt.savefig('brute_force_histogram')
+#     plt.close()
 
-    round_sample = [] # rounds taken for each game will be appended here to be plotted later.
-    for new_game in range(10000):
-        game = AYTO_Game()
-        rounds_taken = greedy_alg(game)
-        round_sample.append(rounds_taken)
-    # Creates a histrogram of rounds taken.
-    sns.histplot(round_sample).set(title='Greedy Algorithm', xlabel='Rounds')
-    # Saves histogram to working directory.
-    plt.savefig('greedy_histogram')
-    plt.close()
+#     round_sample = [] # rounds taken for each game will be appended here to be plotted later.
+#     for new_game in range(10000):
+#         game = AYTO_Game()
+#         rounds_taken = greedy_alg(game)
+#         round_sample.append(rounds_taken)
+#     # Creates a histrogram of rounds taken.
+#     sns.histplot(round_sample).set(title='Greedy Algorithm', xlabel='Rounds')
+#     # Saves histogram to working directory.
+#     plt.savefig('greedy_histogram')
+#     plt.close()
 
-    """
-    round_sample = [] # rounds taken for each game will be appended here to be plotted later.
-    for new_game in range(10000):
-        game = AYTO_Game()
-        rounds_taken = optimized_alg(game)
-        round_sample.append(rounds_taken)
-    # Creates a histrogram of rounds taken.
-    sns.histplot(round_sample).set(title='Optimized Algorithm', xlabel='Rounds')
-    # Saves histogram to working directory.
-    plt.savefig('optimized_histogram')
-    plt.close()
-    """
+#     """
+#     round_sample = [] # rounds taken for each game will be appended here to be plotted later.
+#     for new_game in range(10000):
+#         game = AYTO_Game()
+#         rounds_taken = optimized_alg(game)
+#         round_sample.append(rounds_taken)
+#     # Creates a histrogram of rounds taken.
+#     sns.histplot(round_sample).set(title='Optimized Algorithm', xlabel='Rounds')
+#     # Saves histogram to working directory.
+#     plt.savefig('optimized_histogram')
+#     plt.close()
+#     """
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
